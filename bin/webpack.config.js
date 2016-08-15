45cf83b118be4737200b02bc9b2cc97ea0fe9c48
NeZeD [Mac Air] <nezed@ya.ru>
2016-08-15 19:26:29 +0300

    fixup! Fix webpack-dev-middleware publicPath

--- bin/webpack.config.js
+++ bin/webpack.config.js
@@ -20,9 +20,8 @@ module.exports = {
   entry: path.resolve(projectRoot, SRC_DIRNAME, 'main.js'),
 
   output: {
-    filename: "main.js",
+    filename: "js/main.js",
     path: path.resolve(projectRoot, OUTPUT_DIRNAME, 'js'),
-    publicPath: path.resolve(projectRoot, OUTPUT_DIRNAME),
     outputPath: '/',
     sourceMapFilename: "[file].map"
   }
