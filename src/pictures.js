c277d02abd283afc48c6cc6768fe3450edfd2822
Viacheslav Shestakov <slavassh@gmail.com>
2016-08-22 17:28:11 +0500

    Добавлен перебор загрженного массива изображений с добавлением клонов из шаблона в DOM

--- src/pictures.js
+++ src/pictures.js
@@ -5,10 +5,13 @@
 'use strict';
 
 var pictures = [];
+var filtersMenuForm = document.forms[0];
+console.log(filtersMenuForm);
+debugger;
 
-var getData = function(callData) {
-  return callData;
-};
+var picturesContainer = document.querySelector('.pictures');
+var elemTemplate = document.querySelector('#picture-template');
+console.log(elemTemplate);
 
 var createCallback = function(src, func) {
   var elemScript = document.createElement('script');
@@ -17,8 +20,43 @@ var createCallback = function(src, func) {
 
   window.JSONPCallback = function(data) {
     pictures = func(data);
-    return pictures;
   };
 };
 
-createCallback('api/pictures?callback=JSONPCallback', getData);
+var getData = function(callData) {
+  pictures = callData;
+
+  filtersMenuForm.classList.add('hidden');
+
+  pictures.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
+
+  return pictures;
+};
+
+function createPhoto() {
+  createCallback('api/pictures?callback=JSONPCallback', getData);
+  console.log(pictures);
+  debugger;
+}
+createPhoto();
+
+var elemToClone;
+
+if ('content' in elemTemplate) {
+  elemToClone = elemTemplate.content.querySelector('.picture');
+} else {
+  elemToClone = elemTemplate.querySelector('.picture');
+}
+
+var getPictureElement = function(data, container) {
+  var elem = elemToClone.cloneNode(true);
+  container.appendChild(elem);
+  return elem;
+  console.log(elem);
+};
+
+
+
+
