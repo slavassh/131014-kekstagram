fa724107e0d82472f8e335061887028f15a7fbe0
Viacheslav Shestakov <slavassh@gmail.com>
2016-08-28 23:07:40 +0500

    Подготовка для модуля галереи

---
+++ src/gallery.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Viacheslav Shestakov on 28.08.2016.
+ */
+'use strict';
+
+define(function() {
+  var Gallery = function() {
+
+  }
+
+  Gallery.prototype = {};
+
+  return new Gallery();
+});
