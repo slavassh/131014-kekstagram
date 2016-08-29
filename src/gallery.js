1a5ccfc4de314c43a82903ec9939dee7306ae2eb
Viacheslav Shestakov <slavassh@gmail.com>
2016-08-29 23:31:17 +0500

    Переделаны изменения прототипа

--- src/gallery.js
+++ src/gallery.js
@@ -5,29 +5,50 @@
 
 define(function() {
   var Gallery = function() {
+    var self = this;
     this.galleryOverlay = document.querySelector('.gallery-overlay');
     this.galleryOverlayClose = document.querySelector('.gallery-overlay-close');
     this.galleryOverlayImage = document.querySelector('.gallery-overlay-image');
+    this.commentsCount = document.querySelector('.comments-count');
+    this.likesCount = document.querySelector('.likes-count');
+
+    this.galleryOverlayImage.onclick = function() {
+      self.onImageClick;
+    };
+
+    this.galleryOverlayClose.onclick = function() {
+      self.onCloseClick;
+    };
+
 
   };
 
-  Gallery.prototype = {
-    setPictures: function(picturesArr) {
-      this.pictures = picturesArr;
-      console.log(this.pictures);
-    },
-    show: function(activeNumber) {
-      this.galleryOverlay.classList.remove('invisible');
-      this.setActivePicture(activeNumber);
-    },
-    hide: function() {
-      console.log(this.galleryOverlay);
-    },
-    setActivePicture: function(activeNumber) {
-      this.activePicture = activeNumber;
-      console.log(this.activePicture);
-      debugger;
-    }
+  Gallery.prototype.setPictures = function(picturesArr) {
+    this.pictures = picturesArr;
+  };
+
+  Gallery.prototype.show = function(activeNumber) {
+    this.galleryOverlay.classList.remove('invisible');
+    this.setActivePicture(activeNumber);
+  };
+
+  Gallery.prototype.hide = function() {
+    this.galleryOverlay.classList.add('invisible');
+  };
+
+  Gallery.prototype.setActivePicture = function(activeNumber) {
+    this.activePicture = activeNumber;
+    this.galleryOverlayImage.src = this.pictures[this.activePicture].url;
+    this.commentsCount.textContent = this.pictures[this.activePicture].comments;
+    this.likesCount.textContent = this.pictures[this.activePicture].likes;
+  };
+
+  Gallery.prototype.onImageClick = function() {
+    this.hide();
+  };
+
+  Gallery.prototype.onCloseClick = function() {
+    this.setActivePicture();
   };
 
   return new Gallery();
